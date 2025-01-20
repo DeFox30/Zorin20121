8277ed995a9a02afa8334952adb439f1d38b137f
DeFox30 <paflik28@gmail.com>
2025-01-20 23:17:06 +0300

    Update laba 2.py

--- laba 2.py
+++ laba 2.py
@@ -46,6 +46,19 @@ def threats(board, N):
 
     return threat_board
 
+def print_board(board, N):
+    threat_board = threats(board, N)
+
+    for i in range(N):
+        row = ''
+        for j in range(N):
+            if board[i][j] == 1:
+                row += '# '
+            elif threat_board[i][j] == '*':
+                row += '* '
+            else:
+                row += '0 '
+        print(row.strip())
 
 # Ввод данных
 N, L, K = map(int, input("N(размер доски), L(фигуры для размещения), K(фигуры на доске): ").split())
@@ -61,11 +74,7 @@ for _ in range(K):
 solutions = []
 place_verblud(board, N, L, solutions, positions)
 
-# Вывод всех решений
-if not solutions:
-    print("no solutions")
-else:
-    for solution in solutions:
-        print(', '.join(f"({x},{y})" for x, y in solution))
+# Вывод доски
+print_board(board, N)
 
 print("Общее количество решений:", len(solutions))
